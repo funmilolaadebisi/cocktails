6f8864a08546805e234f3baa08a1888701f8414e
Lola <elizabeth.tamera@gmail.com>
2023-06-29 15:02:21 +0100

    handle submit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,15 +7,19 @@ const SearchForm = () => {
 
   React.useEffect(() => {
     searchValue.current.focus();
-  });
+  }, []);
 
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <article className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name"> search for your favorite cocktails </label>
           <input
