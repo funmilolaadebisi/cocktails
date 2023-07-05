020b9787de307153d4872a5752fc7583c7acd602
Lola <elizabeth.tamera@gmail.com>
2023-07-05 08:31:33 +0100

    a commit

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Cocktail = ({ image, name, id, info, glass}) => {
+const Cocktail = ({ image, name, id, info, glass }) => {
   return (
     <article className="cocktail">
       <div className="img-container">
